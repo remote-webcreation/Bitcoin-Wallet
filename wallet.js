2c5f975cbc20f36205fdc50560b35b4dd7784a4d
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-07-12 15:28:15 +0200

    generated testnet-adress + private key saved in mywallet.json

---
+++ wallet.js
@@ -0,0 +1,32 @@
+import * as bitcoin from 'bitcoinjs-lib';
+import { ECPairFactory } from 'ecpair';
+import * as ecc from 'tiny-secp256k1';
+// Netzwerk: Testnet statt Mainnet!
+import { writeFileSync } from 'fs';
+
+const ECPair = ECPairFactory(ecc);
+
+const network = bitcoin.networks.testnet;
+// Schlüsselpaar generieren
+const keyPair = ECPair.makeRandom({ network });
+
+// WIF = "Wallet Import Format" (für Backup)
+const privateKey = keyPair.toWIF();
+const { address } = bitcoin.payments.p2pkh({
+    pubkey: Buffer.from(keyPair.publicKey),
+    network,
+});
+
+console.log("🚀 Deine Testnet-Adresse:", address);
+console.log("🔐 Dein privater Schlüssel (WIF):", privateKey);
+
+// ❗ Niemals keys/mywallet.json in Git tracken oder hochladen!
+//    Dieser Ordner enthält sensible private Schlüssel und sollte
+//    immer in deiner .gitignore stehen.
+
+const walletData = {
+    address,
+    privateKey,
+}
+
+writeFileSync('keys/mywallet.json', JSON.stringify(walletData, null, 2));
